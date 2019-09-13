67004f86164f46c617e7f2b2e6e2649284f4ed41
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-13 14:02:18 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> print('Hello World')
+Hello World
+>>> 
