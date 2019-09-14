12d167f49eac5ce043a4562ce8d2cf91be75e3f5
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:05:26 +0530

    Add files via upload
    values

---
+++ Values.py
@@ -0,0 +1,16 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> print(4)
+4
+>>> type(4)
+<type 'int'>
+>>> print('Hello World')
+Hello World
+>>> type('Hello World')
+<type 'str'>
+>>> print(12.90)
+12.9
+>>> type(12.90)
+<type 'float'>
+>>> 
