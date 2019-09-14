374dbe620344fa5419615789279e4e54849e9d7f
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:07:47 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,20 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> a = 12
+>>> b = 78
+>>> print(a+b)
+90
+>>> print(a-b)
+-66
+>>> print(a*b)
+936
+>>> print(a/b)
+0
+>>> print(a//b)
+0
+>>> print(a%b)
+12
+>>> print(a**b)
+1500158654173824244445901346699938405046228479331843405109463057079932580570809237504
+>>> 
