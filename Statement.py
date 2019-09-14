052e59ff0434b9bf62ef69ce88362b96dd8a8536
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:07:00 +0530

    Add files via upload

---
+++ Statement.py
@@ -0,0 +1,9 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> print(12)
+12
+>>> x = 12
+>>> print(x)
+12
+>>> 
