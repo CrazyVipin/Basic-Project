4419ac7419c603b1afb30e7efb50958810b8dfe6
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:08:27 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> val = input('Enter the Number are\n')
+Enter the Number are
+233
+>>> per = val + 12
+>>> print(per)
+245
+>>> 
