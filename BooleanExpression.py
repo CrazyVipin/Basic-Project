27d7cc8eadca36cf44fe99ca3c8baa88a526ac71
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:09:06 +0530

    Add files via upload

---
+++ BooleanExpression.py
@@ -0,0 +1,22 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> x = 12
+>>> y = 13
+>>> x == y
+0
+>>> x != y
+1
+>>> x > y
+0
+>>> x < y
+1
+>>> x <= y
+1
+>>> x >= y
+0
+>>> x is y
+0
+>>> x is not y
+1
+>>> 
