bd76682960fb154ed22659f68c5f1a02ac657d31
CrazyVipin <54491664+CrazyVipin@users.noreply.github.com>
2019-09-14 21:06:09 +0530

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,19 @@
+Python 2.2.3 (#42, May 30 2003, 18:12:08) [MSC 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license" for more information.
+IDLE 0.8 -- press F1 for help
+>>> message = 'Hello World '
+>>> print(message)
+Hello World 
+>>> n = 12
+>>> print(n)
+12
+>>> pi = 3.14
+>>> print(pi)
+3.14
+>>> type(message)
+<type 'str'>
+>>> type(n)
+<type 'int'>
+>>> type(pi)
+<type 'float'>
+>>> 
